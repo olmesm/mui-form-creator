b80501e84022f73c88406a660e0ea9c2efd9207d
olmesm <olmesm@users.noreply.github.com>
2023-05-16 19:37:51 +0000

    deploy: 4705a18610b9cc2a1861141411d21e9eff5027a4

---
+++ assets/Layout-e1bcc9af.js
@@ -0,0 +1,1 @@
+import{j as t,O as o}from"./index-7c906ef2.js";const e=()=>t.jsx(t.Fragment,{children:t.jsx(o,{})});export{e as Component};
--- assets/Layout-ef4f2997.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as o}from"./index-cf494745.js";const e=()=>t.jsx(t.Fragment,{children:t.jsx(o,{})});export{e as Component};
